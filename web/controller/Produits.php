da7f8fa2794b0ffec595561ebc98661c4ea195c4
Marion HEUTTE <m.heutte@outlook.fr>
2015-12-07 17:33:26 +0100

    EditerProduit

--- web/controller/Produits.php
+++ web/controller/Produits.php
@@ -4,7 +4,7 @@
 */
 class Produits extends Controller
 {	
-	public function ajouterProduit()
+	public function ajouter()
 	{
 		$db = App::getDatabase();
 		$categorie = $db->query('Select idcategorieproduit, nomcategorieproduit from categorieproduit');
@@ -29,5 +29,44 @@ class Produits extends Controller
 		    	'success' => isset($success) ? $success : null
 		    ));
 	}
-			
+	
+	public function editer($idProduit)
+	{
+		$db = App::getDatabase();
+
+		$produit = $db->query('Select * from produit where idproduit = ?', array($idProduit));
+		$resultp['editerProduit'] = $produit->fetch(PDO::FETCH_ASSOC);
+		$this->render('editerProduit', [
+			'produitmodif' => $resultp
+		]);
+
+		var_dump($resultp);
+
+		$categorie = $db->query('Select idcategorieproduit, nomcategorieproduit from categorieproduit');
+		$result = $categorie->fetchAll(PDO::FETCH_ASSOC);
+		$catproduit = array();
+
+		foreach ($result as $cat) {
+			$catproduit[$cat['nomcategorieproduit']] = $cat['idcategorieproduit'];
+		}
+
+
+			if($_SERVER['REQUEST_METHOD'] === 'POST') 
+			{
+					$data = $_POST['ajouterProduit'];
+					$req = $db->query('Update Produit set nomProduit = ?,
+														  prix = ?,
+														  stock = ?,
+														  categorieProduit = ?' , 
+										array($data['nomproduit'],$data['prix'],$data['stock'],$data['categorieProduit']));
+
+					$success = "Produit ".$data['nomproduit']." modifié !";
+		    }
+
+		$this->render('editerProduit', array(
+		    	'catproduit' => $catproduit,
+		    	'resultp' => $resultp,
+		    	'success' => isset($success) ? $success : null
+		    ));
+	}		
 }
